53266c5200ebbaebf3a24eee0df79d86773ac88d
Alok Kumar <alokkumar0723@gmail.com>
2023-07-23 14:02:10 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "da3cf9351efe110d707de3a49db5914b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
